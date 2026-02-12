243ed51c4fdfa536bf478b2633a8ada143305ecc
louis0113 <luizhenrique011304@gmail.com>
2026-02-12 14:51:57 -0300

    fix: more clean connect of databases

--- app.js
+++ app.js
@@ -5,27 +5,18 @@ import redis from "./src/config/redis.js"
 import {uri, mongoose} from "./src/config/mongodb.js"
 const app = express();
 
-
+async function databaseConnect(callback, database){
 try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+  await callback;
+  console.log(`Conectado com sucesso ao ${database}`);
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error(`Erro ao conectar com ${database}:`, error);
+}
 }
 
-redis.on('error', err => console.log('Redis Client Error', err));
-
-await redis.connect()
-
-mongoose.connect(uri, {
-    serverSelectionTimeoutMS: 5000 
-})
-.then(() => console.log('Banco Conectado com SUCESSO!'))
-.catch(err => {
-    console.error(' ERRO CRÍTICO DE CONEXÃO:');
-    console.error('Motivo:', err.message); 
-    console.error('Verifique: 1. Senha na string; 2. IP no Network Access (0.0.0.0/0)');
-});
+databaseConnect(sequelize.sync(), 'MySQL');
+databaseConnect(redis.connect(), 'Redis');
+databaseConnect(mongoose.connect(uri), 'MongoDB');
 
 export default app;
 
