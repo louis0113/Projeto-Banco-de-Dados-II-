1c1e09838c135a447e27123bf38072abd7fc274e
louis0113 <luizhenrique011304@gmail.com>
2026-02-18 19:05:50 -0300

    fix: bullshit code

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ import express from 'express';
 import 'dotenv/config';
 import sequelize from "./src/config/sequelize.js";
 import redis from "./src/config/redis.js"
-import {uri, mongoose} from "./src/config/mongo.js"
+import {uri, mongoose} from "./src/config/mongodb.js"
 
 const app = express();
 app.use(express.json());
 
 async function databaseConnect(callback, database){
-try {
-  await sequelize.authenticate();
-  console.log("MySQL connection established successfully.");
-} catch (error) {
-  console.error("Unable to connect to MySQL:", error);
+    try{
+        await callback
+        console.log(`Connect from ${database}`)
+    } catch (err){
+        console.error(`Unable to connect from ${database} `, err)
+    }
 }
 
 databaseConnect(sequelize.sync(), 'MySQL');
 databaseConnect(redis.connect(), 'Redis');
 databaseConnect(mongoose.connect(uri), 'MongoDB');
 
-await connectMongo();
 export default app;
